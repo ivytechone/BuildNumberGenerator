009264bcfe6fa87a5a7d24521fd6dea136c00d87
Robert <107894728+ivytechone@users.noreply.github.com>
2022-07-06 19:56:33 -0700

    skeleton for controller

---
+++ BuildNumberGenerator/Controllers/GetBuildNumberController.cs
@@ -0,0 +1,20 @@
+namespace BuildNumberGenerator.Controllers
+{
+   	[ApiController]
+	[Route("api/getBuildNumber/")]
+	public class GetBuildNumberController : ControllerBase
+	{ 
+        private ILogger<GetBuildNumberController> _logger;
+
+        public GetBuildNumberController(ILogger<GetBuildNumberController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new OkResult();
+        }
+    }
+}
